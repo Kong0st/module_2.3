b374625c7892765329eb4151b5710048bcd79330
Kong0st <kongost@yandex.ru>
2024-06-15 20:04:43 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -7,12 +7,29 @@
 #     print(num)  # Выводим положительные числа
 
 
+# my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+# index = 0
+#
+# while index < len(my_list):
+#     num = my_list[index]
+#     if num < 0:
+#         break
+#     print(num)
+#     index += 1
+
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 
+# Начинаем цикл while
 while index < len(my_list):
-    num = my_list[index]
-    if num < 0:
+    # Если число отрицательное, прерываем цикл
+    if my_list[index] < 0:
         break
-    print(num)
-    index += 1  # Иначе зациклица на 1 числе
+    # Если число равно нулю, пропускаем и переходим к следующему элементу списка
+    elif my_list[index] == 0:
+        index += 1
+        continue
+    # Печатаем число, если оно положительное
+    print(my_list[index])
+    # Переходим к следующему элементу списка
+    index += 1
